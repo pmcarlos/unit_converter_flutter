6d6e0e83161f4831280ac0999af407af5e310113
pmcarlos <carlos@passaromx.com>
2019-02-15 10:04:17 -0600

    Added covnersion input and output

--- lib/category.dart
+++ lib/category.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
+import 'package:unit_converter_app/converter_screen.dart';
+import 'package:unit_converter_app/unit.dart';
+
 final _rowHeight = 100.0;
 final _borderRadius = BorderRadius.circular(_rowHeight / 2);
 
@@ -8,16 +11,39 @@ class Category extends StatelessWidget {
   final String name;
   final ColorSwatch color;
   final IconData iconLocation;
+  final List<Unit> units;
 
   const Category(
       {Key key,
       @required this.name,
       @required this.color,
-      @required this.iconLocation})
+      @required this.iconLocation,
+      @required this.units})
       : assert(name != null),
         assert(color != null),
         assert(iconLocation != null),
+        assert(units != null),
         super(key: key);
+
+  void _navigateToConverter(BuildContext context) {
+    print('$color, $name, $units');
+    Navigator.of(context)
+        .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          elevation: 1.0,
+          title: Text(
+            name,
+            style: Theme.of(context).textTheme.display1,
+          ),
+          centerTitle: true,
+          backgroundColor: color,
+        ),
+        body: ConverterScreen(color: color, name: name, units: units),
+        resizeToAvoidBottomPadding: true,
+      );
+    }));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,11 +53,9 @@ class Category extends StatelessWidget {
         height: _rowHeight,
         child: InkWell(
           borderRadius: _borderRadius,
-          highlightColor: color,
-          splashColor: color,
-          onTap: () {
-            print('I was tapped');
-          },
+          highlightColor: color['highlight'],
+          splashColor: color['splash'],
+          onTap: () => _navigateToConverter(context),
           child: Padding(
             padding: EdgeInsets.all(8.0),
             child: Row(
